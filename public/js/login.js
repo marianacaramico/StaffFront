fc31cf308cfad86d0ce31651612132577c6d66e3
= <=>
2018-03-08 08:24:33 -0300

    updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -5,26 +5,19 @@
     if ($("#inputEmail").val() === "") {
       $("#inputEmail").addClass("textBoxError");
       $("#helpBLockEmail").text("Digite o seu email");
-      ret = false;
+      e.preventDefault();
     } else {
       $("#inputEmail").removeClass("textBoxError");
       $("#helpBLockEmail").text();
-      ret = ret && true;
     }
 
     if ($("#inputSenha").val() === "") {
       $("#inputSenha").addClass("textBoxError");
       $("#helpBLockSenha").text("Digite a sua senha");
       e.preventDefault();
-      ret = false;
     } else {
       $("#inputSenha").removeClass("textBoxError");
       $("helpBLockSenha").text();
-      ret = ret && true;
-    }
-
-    if (!passed) {
-       e.preventDefault()
     }
 
   });
