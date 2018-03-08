a064273b47651d14fd4df1a8493665c81eb9f687
= <=>
2018-03-08 08:40:33 -0300

    login.js login.ejs updates

--- public/js/login.js
+++ public/js/login.js
@@ -2,30 +2,27 @@
 (function() {
   $("#loginForm").submit((e) => {
     if ($("#inputEmail").val() === "") {
-      $("#inputEmail").addClass("textBoxError");
-      $("#helpBLockEmail").text("Digite o seu email");
       e.preventDefault();
+      $("#inputEmail").addClass("textBoxError");
+      $("#helpBLockEmail").show();
     } else {
       $("#inputEmail").removeClass("textBoxError");
-<<<<<<< HEAD
-      $("#helpBLockEmail").text("");
-=======
-      $("#helpBLockEmail").text();
->>>>>>> fc31cf308cfad86d0ce31651612132577c6d66e3
+      $("#helpBLockEmail").hide();
     }
 
     if ($("#inputSenha").val() === "") {
-      $("#inputSenha").addClass("textBoxError");
-      $("#helpBLockSenha").text("Digite a sua senha");
       e.preventDefault();
+      $("#inputSenha").addClass("textBoxError");
+      $("#helpBLockSenha").show();
     } else {
       $("#inputSenha").removeClass("textBoxError");
-<<<<<<< HEAD
-      $("helpBLockSenha").text("");
-=======
-      $("helpBLockSenha").text();
->>>>>>> fc31cf308cfad86d0ce31651612132577c6d66e3
+      $("helpBLockSenha").hide();
     }
+
+    $document.ready(() => {
+      $("#helpBLockEmail").hide();
+      $("#helpBLockSenha").hide();
+    });
 
   });
 })();
