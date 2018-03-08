28d070bde12fd29cc106b17b07ee4d9b105dbe14
= <=>
2018-03-08 07:36:47 -0300

    Login page updates

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+"use strict";
+(function() {
+  $("#loginButton").click((e) => {
+    if (isNull($("#inputEmail"))) {
+      $("#inputEmail").addClass("textBoxError");
+      $("#helpBLockEmail").text("Digite o seu email");
+      e.preventDefault();
+    } else {
+      $("#inputEmail").removeClass("textBoxError");
+      $("#helpBLockEmail").text();
+    }
+
+    if (isNull($("#inputSenha"))) {
+      $("#inputSenha").addClass("textBoxError");
+      $("#helpBLockSenha").text("Digite a sua senha");
+      e.preventDefault();
+    } else {
+      $("#inputSenha").removeClass("textBoxError");
+      $("helpBLockSenha").text();
+    }
+  });
+})();
