326ee32a01732fab3295cc5e4b97a20eba735b65
Mateus Larrubia <mateus.larrubia99@gmail.com>
2018-06-04 02:06:22 -0300

    Reformulacao do perfil de usuario

--- models/User.js
+++ models/User.js
@@ -2,18 +2,27 @@ var bcrypt = require('bcrypt');
 var TYPES = require('tedious').TYPES;
 var Database = require('../helpers/Database');
 
+function validFunction(param) {
+    if (typeof param === "function") {
+        return param;
+    } else {
+        return (function(){});
+    }
+}
+
 function login(username, password, callbackFunctions = {}) {
     var database = new Database();
     var connection = database.connect();
 
+    callbackFunctions.onSuccess = validFunction(callbackFunctions.onSuccess);
+    callbackFunctions.onFail = validFunction(callbackFunctions.onFail);
+
     connection.on('connect', function(err) {
         if (err) {
-            if (typeof callbackFunctions.onFail === "function") {
-                callbackFunctions.onFail(err, {
-                    code: 0,
-                    result: 'Failed to connect to database'
-                });
-            }
+            callbackFunctions.onFail(err, {
+                code: 0,
+                result: 'Failed to connect to database'
+            });
         } else {
             var result = {};
             var query = "SELECT @id = id_user, @password = password FROM dbo.TB_USER WHERE username = @username";
@@ -29,32 +38,68 @@ function login(username, password, callbackFunctions = {}) {
                     if (comparison) {
                         var id = Number(result.id) || 0;
                         if (id) {
-                            if (typeof callbackFunctions.onSuccess === "function") {
-                                callbackFunctions.onSuccess({
-                                    code: 1,
-                                    result: {
-                                        redirect: '/home',
-                                        session_id: id
-                                    }
-                                });
-                            }
+                            callbackFunctions.onSuccess({
+                                code: 1,
+                                result: {
+                                    redirect: '/home',
+                                    session_id: id
+                                }
+                            });
                         } else {
-                            if (typeof callbackFunctions.onFail === "function") {
-                                callbackFunctions.onFail(err, {
-                                    code: 2,
-                                    result: 'Invalid username or password'
-                                });
-                            }
-                        }
-                    } else {
-                        if (typeof callbackFunctions.onFail === "function") {
                             callbackFunctions.onFail(err, {
                                 code: 2,
                                 result: 'Something went wrong'
                             });
                         }
+                    } else {
+                        callbackFunctions.onFail(err, {
+                            code: 2,
+                            result: 'Email ou senha inválidos'
+                        });
                     }
                 });
+            });
+            connection.execSql(request);
+        }
+    });
+}
+
+function getUser(userid, callbackFunctions = {}) {
+    var database = new Database();
+    var connection = database.connect();
+
+    callbackFunctions.onSuccess = validFunction(callbackFunctions.onSuccess);
+    callbackFunctions.onFail = validFunction(callbackFunctions.onFail);
+
+    connection.on('connect', function (err) {
+        if (err) {
+            callbackFunctions.onFail(err, {
+                code: 0,
+                result: "Failed to connect to database"
+            });
+        } else {
+            var queryString = "SELECT @id = id_user, @name = name, @username = username FROM dbo.TB_USER WHERE id_user = @userid";
+            var request = database.query(queryString, connection);
+            var result = {};
+            request.addParameter('userid', TYPES.Int, userid);
+            request.addOutputParameter('id', TYPES.Int);
+            request.addOutputParameter('name', TYPES.VarChar);
+            request.addOutputParameter('username', TYPES.VarChar);
+            request.on('returnValue', function(parameterName, value, metadata) {
+                result[parameterName] = value;
+            });
+            request.on('requestCompleted', function() {
+                if (result.id) {
+                    callbackFunctions.onSuccess({
+                        code: 1,
+                        result: result
+                    });
+                } else {
+                    callbackFunctions.onSuccess({
+                        code: 0,
+                        result: "Usuário não encontrado"
+                    });
+                }
             });
             connection.execSql(request);
         }
@@ -64,6 +109,9 @@ function login(username, password, callbackFunctions = {}) {
 function signup(name, email, password, cpf, cep, rua, numero, bairro, cidade, estado, callbackFunctions = {}) {
     var database = new Database();
     var connection = database.connect();
+
+    callbackFunctions.onSuccess = validFunction(callbackFunctions.onSuccess);
+    callbackFunctions.onFail = validFunction(callbackFunctions.onFail);
 
     connection.on('connect', function(err) {
         if (err) {
@@ -78,40 +126,32 @@ function signup(name, email, password, cpf, cep, rua, numero, bairro, cidade, es
                 var queryVerify = "SELECT * FROM dbo.TB_USER WHERE username = @username";
                 var requestVerify = database.query(queryVerify, connection, function(err, rowCount, rows) {
                     if (err) {
-                        if (typeof callbackFunctions.onFail === "function") {
-                            callbackFunctions.onFail(err, {
-                                code: 0,
-                                result: "Something went wrong"
-                            });
-                        }
+                        callbackFunctions.onFail(err, {
+                            code: 0,
+                            result: "Something went wrong"
+                        });
                     } else if (!rowCount) {
                         var query = "INSERT INTO dbo.TB_USER(name, username, password, cpf, cep, rua, numero, bairro, cidade, estado) VALUES (@name, @username, @password, @cpf, @cep, @rua, @numero, @bairro, @cidade, @estado); SELECT @@identity";
                         var request = database.query(query, connection, function(err, rowCount, rows) {
+                            connection.close();
                             if (err) {
-                                if (typeof callbackFunctions.onFail === "function") {
-                                    callbackFunctions.onFail(err, {
-                                        code: 0,
-                                        result: "Something went wrong"
-                                    });
-                                }
+                                callbackFunctions.onFail(err, {
+                                    code: 0,
+                                    result: "Something went wrong"
+                                });
                             } else {
                                 if (rowCount) {
-                                    if (typeof callbackFunctions.onSuccess === "function") {
-                                        callbackFunctions.onSuccess({
-                                            code: 1,
-                                            result: "Usuário cadastrado com sucesso"
-                                        });
-                                    }
+                                    callbackFunctions.onSuccess({
+                                        code: 1,
+                                        result: "Usuário cadastrado com sucesso"
+                                    });
                                 } else {
-                                    if (typeof callbackFunctions.onSuccess === "function") {
-                                        callbackFunctions.onSuccess({
-                                            code: 0,
-                                            result: "Failed to create new user"
-                                        });
-                                    }
+                                    callbackFunctions.onSuccess({
+                                        code: 0,
+                                        result: "Failed to create new user"
+                                    });
                                 }
                             }
-                            connection.close();
                         });
                         request.addParameter('name', TYPES.VarChar, name);
                         request.addParameter('username', TYPES.VarChar, email);
@@ -126,12 +166,10 @@ function signup(name, email, password, cpf, cep, rua, numero, bairro, cidade, es
                         connection.execSql(request);
                     } else {
                         connection.close();
-                        if (typeof callbackFunctions.onSuccess === "function") {
-                            callbackFunctions.onSuccess({
-                                code: 0,
-                                result: "E-mail já cadastrado"
-                            });
-                        }
+                        callbackFunctions.onSuccess({
+                            code: 0,
+                            result: "E-mail já cadastrado"
+                        });
                     }
                 });
                 requestVerify.addParameter("username", TYPES.VarChar, email);
@@ -143,6 +181,7 @@ function signup(name, email, password, cpf, cep, rua, numero, bairro, cidade, es
 
 var User = {
     login,
+    getUser,
     signup
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var TYPES = require('tedious').TYPES;
-var Database = require('../helpers/Database');
+var Task = require('../models/Task');
+var User = require('../models/User');
 
 router.get('*', function (req, res, next) {
     if (!req.session.userid) {
@@ -13,37 +13,54 @@ router.get('*', function (req, res, next) {
 
 /* GET USER */
 router.get('/:id', function (req, res, next) {
-    var id = parseInt(req.params.id) || 0;
-    var result = {};
+    var userid = parseInt(req.params.id) || 0;
+    var myUserid = parseInt(req.session.userid);
 
-    var database = new Database();
-    var connection = database.connect();
-    connection.on('connect', function(err) {
-        if (err) {
+    if (!myUserid) {
+        return next();
+    }
+
+    User.getUser(userid, {
+        onSuccess: function onSuccess(response) {
+            if (Number(response.code) === 1) {
+                Task.getUnassigned(userid, {
+                    onSuccess: function taskSuccess(responseSuccess) {
+                        Task.getTasksFinishedByMe(userid, {
+                            onSuccess: function onInnerSuccess(innerSuccess) {
+                                res.render('user', {
+                                    title: response.result.name + " - Staff",
+                                    css: ["user", "tasks"],
+                                    script: "search",
+                                    result: response.result,
+                                    isMyProfile: userid === myUserid,
+                                    openTasks: responseSuccess.tasks || [{
+                                        title: "Nenhuma tarefa encontrada",
+                                        description: "Nenhuma tarefa pôde ser encontrada."
+                                    }],
+                                    finishedTasks: innerSuccess.tasks || [{
+                                        title: "Nenhuma tarefa encontrada",
+                                        description: "Nenhuma tarefa pôde ser encontrada."
+                                    }]
+                                });
+                            },
+                            onFail: function onInnerFail(err, responseJson) {
+                                console.log(err);
+                                next();
+                            }
+                        });
+                    },
+                    onFail: function taskFail(err, responseJson) {
+                        console.log(err);
+                        next();
+                    }
+                });
+            } else {
+                next();
+            }
+        },
+        onFail: function onFail(err, responseJson) {
             console.log(err);
             next();
-        } else {
-            var queryString = "SELECT @id = id_user, @name = name, @username = username FROM dbo.TB_USER WHERE id_user = " + id;
-            var request = database.query(queryString, connection);
-            request.addOutputParameter('id', TYPES.Int);
-            request.addOutputParameter('name', TYPES.VarChar);
-            request.addOutputParameter('username', TYPES.VarChar);
-            request.on('returnValue', function(parameterName, value, metadata) {
-                result[parameterName] = value;
-            });
-            request.on('requestCompleted', function() {
-                if (result.id) {
-                    res.render('user', {
-                        title: result.name + " - Staff",
-                        css: "user",
-                        script: "tasks",
-                        result: result
-                    });
-                } else {
-                    next();
-                }
-            });
-            connection.execSql(request);
         }
     });
 });
