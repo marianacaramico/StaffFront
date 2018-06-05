32fb0bca6743b07c55ce15ca7b09ec7530c2c827
MateusLarrubia <mateus@publitz.com.br>
2018-06-05 12:32:05 -0300

    Adicionado link para perfil no navbar

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.locals.pUserid = req.session.userid;
+  next();
+});
 
 app.use('*', function(req, res, next) {
   if (req.app.get('env') !== 'development') {
